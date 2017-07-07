3805a3f1561cc2ed7974dea0b20fa657f2e7514f
Christian Leo-Pernold <mazedlx@gmail.com>
2017-07-07 21:49:42 +0200

    cleanup, modified readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # vue-checkbox-toggle
-A VueJS checkbox replacment thing. 
+A simple VueJS checkbox replacment thing.
+
+## Installation
+
+First, install it with NPM
+`npm install mazedlx/vue-checkbox-toggle`
+
+Then, import it where needed
+`import vue-checkbox-toggle from 'vue-checkbox-toggle';`
--- VueCheckbox.vue
+++ VueCheckbox.vue
@@ -1,18 +1,18 @@
 <template>
-    <div 
-        class="checkbox-toggle" 
-        role="checkbox" 
-        @keydown="toggle" 
-        @click.stop="toggle" 
-        tabindex="0" 
+    <div
+        class="checkbox-toggle"
+        role="checkbox"
+        @keydown="toggle"
+        @click.stop="toggle"
+        tabindex="0"
         :aria-checked="toggled"
     >
-        <div 
-            class="checkbox-slide" 
+        <div
+            class="checkbox-slide"
             :class="classes"
         >
-            <div 
-                class="checkbox-switch" 
+            <div
+                class="checkbox-switch"
                 :class="classes"
             ></div>
         </div>
@@ -52,9 +52,7 @@ export default {
             if (this.disabled || e.keyCode === 9) { // not if disabled or tab is pressed
                 e.stop();
             }
-
             this.toggled = ! this.toggled;
-
             this.$emit("input", this.toggled);
         }
     },
@@ -83,73 +81,73 @@ export default {
         labelUnchecked: {
             type: String,
             default: ""
-        }
-    },  
-});
+        },
+    },
+}
 </script>
 
 <style lang="sass" scoped>
-$slideWidth: 4em
-$borderRadius: 0.25em
+    $slideWidth: 4em
+    $borderRadius: 0.25em
 
-$switchWidth: $slideWidth/2
-$switchHeight: $slideWidth/2
+    $switchWidth: $slideWidth/2
+    $switchHeight: $slideWidth/2
 
-$distance: $slideWidth/2
+    $distance: $slideWidth/2
 
-$switchColor: rgb(56, 74, 93)
-$sliderColorActive: rgb(103, 175, 127)
-$sliderColorInactive: rgb(211, 211, 211)
+    $switchColor: rgb(56, 74, 93)
+    $sliderColorActive: rgb(103, 175, 127)
+    $sliderColorInactive: rgb(211, 211, 211)
 
-$transitionTime: 350ms
+    $transitionTime: 350ms
 
-.checkbox-toggle
-  width: $slideWidth * 3
-  display: flex
-  flex-direction: row
-  justify-content: flex-start
-  align-items: center
-  margin: 0.5em
+    .checkbox-toggle
+        width: $slideWidth * 3
+        display: flex
+        flex-direction: row
+        justify-content: flex-start
+        align-items: center
+        margin: 0.5em
 
-.checkbox-slide
-  width: $slideWidth
-  padding: 0
-  margin: 0
-  border-radius: .25em
-  cursor: pointer
-  
-.checkbox-switch 
-  padding: 0
-  margin: 0
-  width: $switchWidth
-  height: $switchHeight
-  border-radius: $borderRadius
-  background: $switchColor
-  cursor: pointer
+    .checkbox-slide
+        width: $slideWidth
+        padding: 0
+        margin: 0
+        border-radius: .25em
+        cursor: pointer
 
-.checkbox-label
-  margin-left: 0.5em
+    .checkbox-switch
+        padding: 0
+        margin: 0
+        width: $switchWidth
+        height: $switchHeight
+        border-radius: $borderRadius
+        background: $switchColor
+        cursor: pointer
 
-.checkbox-switch.checked
-  transform: translateX($distance)
-  transition: all $transitionTime
+    .checkbox-label
+        margin-left: 0.5em
 
-.checkbox-switch.unchecked
-  transition: all $transitionTime
- 
-.checkbox-slide.checked
-  transition: all $transitionTime
-  background: $sliderColorActive
+    .checkbox-switch.checked
+        transform: translateX($distance)
+        transition: all $transitionTime
 
-.checkbox-slide.unchecked
-  transition: all $transitionTime
-  background: $sliderColorInactive  
-  
-.checkbox-switch.disabled
-  cursor: not-allowed
-  background: lighten($switchColor, 35%)
+    .checkbox-switch.unchecked
+        transition: all $transitionTime
 
-.checkbox-slide.disabled
-  cursor: not-allowd
-  background: lighten($sliderColorInactive, 5%)
+    .checkbox-slide.checked
+        transition: all $transitionTime
+        background: $sliderColorActive
+
+    .checkbox-slide.unchecked
+        transition: all $transitionTime
+        background: $sliderColorInactive
+
+    .checkbox-switch.disabled
+        cursor: not-allowed
+        background: lighten($switchColor, 35%)
+
+    .checkbox-slide.disabled
+        cursor: not-allowd
+        background: lighten($sliderColorInactive, 5%)
 </style>
